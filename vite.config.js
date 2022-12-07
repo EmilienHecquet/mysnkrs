d7ba2783251621a1a28c44cdb808f07312ea3c40
Emilien <emilien.hecquet@my-digital-school.org>
2022-12-07 13:59:49 +0100

    deploy github page

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,4 @@
-const path = require('path')
 
 export default {
-  root: path.resolve(__dirname, 'src'),
-  resolve: {
-    alias: {
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-    }
-  },
   base: '/mysnkrs/'
 }
